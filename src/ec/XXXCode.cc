72c3ba4a51c592e42e278d7e16fa200ed2611ce7
Keyun Cheng <keyun_cheng@qq.com>
2024-12-28 23:03:18 +0800

    add code impl (partial)

--- src/ec/XXXCode.cc
+++ src/ec/XXXCode.cc
@@ -1,1 +1,41 @@
 #include "XXXCode.hh"
+
+XXXCode::XXXCode(int n, int k, int w, int opt, vector<string> param) {
+
+}
+
+ECDAG* XXXCode::Encode() {
+
+}
+
+ECDAG* XXXCode::Decode(vector<int> from, vector<int> to) {
+
+}
+
+void XXXCode::Place(vector<vector<int>>& group) {
+
+}
+
+void XXXCode::genParityCheckMatrix() {
+
+}
+
+void XXXCode::genEncodingMatrix() {
+
+}
+
+void XXXCode::convertPCMatrix2EncMatrix() {
+
+}
+
+void XXXCode::genDecodingMatrix(vector<int> &availNodes, vector<int> &failedNodes) {
+
+}
+
+void XXXCode::repairSingle(vector<int> &availNodes, int failedNode) {
+
+}
+
+void XXXCode::repairMultiple(vector<int> &availNodes, vector<int> &failedNodes) {
+
+}
