e9d9d105a6103b24766fdc8a501ab1510d18a254
Keyun Cheng <keyun_cheng@qq.com>
2025-03-07 06:40:50 +0000

    fix script

--- exp_script/set_bw.sh
+++ exp_script/set_bw.sh
@@ -4,18 +4,18 @@
 source "./load_eval_settings.sh"
 
 if [ "$#" != "1" ]; then
-	echo "Usage: $0 upload_bw (Kbps)" >&2
+	echo "Usage: $0 bw (Kbps)" >&2
     exit 1
 fi
 
-upload_bw_Kbps=$1
+bw_Kbps=$1
 
 wondershaper_dir=$home_dir/wondershaper
 
 node_ip=$(ifconfig | grep $ip_prefix | head -1 | sed "s/ *inet [addr:]*\([^ ]*\).*/\1/")
 node_dev=$(ifconfig | grep -B1 $node_ip | grep -o "^\w*")
 
-cd $wondershaper_dir && echo $user_passwd | sudo -S ./wondershaper -a $node_dev -u $upload_bw_Kbps
+cd $wondershaper_dir && echo $user_passwd | sudo -S ./wondershaper -a $node_dev -u $bw_Kbps -d $bw_Kbps
 cd -
 
-echo enable bandwidth setting: $node_ip $node_dev $upload_bw_Kbps Kbps
+echo enable bandwidth setting: $node_ip $node_dev $bw_Kbps Kbps
