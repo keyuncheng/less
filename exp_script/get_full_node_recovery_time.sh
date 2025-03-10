8865655c0fb7ebe0c221bf5e8318a4762a8a3d7c
Keyun Cheng <keyun_cheng@qq.com>
2025-03-10 17:40:20 +0800

    add script for full-node recovery time

---
+++ exp_script/get_full_node_recovery_time.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Usage: calculate the full node recovery time
+
+source "./load_eval_settings.sh"
+
+LOG_FILE=${proj_dir}/coor_output
+START_PATTERN="StripeStore::scanning repair queue"
+END_PATTERN="Coordinator::repair for "
+TARGET_COUNT=20
+COUNT=0
+START_TIME=""
+END_TIME=""
+
+tail -Fn0 "$LOG_FILE" | \
+while read line; do
+    if [[ -z "$START_TIME" && "$line" == *"$START_PATTERN"* ]]; then
+        START_TIME=$(date +%s.%N)
+        echo "[$(date)] ⏱ Start time recorded: $START_TIME"
+    elif [[ -n "$START_TIME" && "$line" == "$END_PATTERN"*finishes ]]; then
+        COUNT=$((COUNT + 1))
+        echo "[$(date)] Match $COUNT/$TARGET_COUNT"
+        END_TIME=$(date +%s.%N)
+        DURATION=$(echo "scale=6; $END_TIME - $START_TIME" | bc -l)
+        echo "[$(date)] Recovery time for $COUNT OEC objects: $DURATION seconds"
+        if [[ "$COUNT" -eq "$TARGET_COUNT" ]]; then
+            END_TIME=$(date +%s.%N)
+            DURATION=$(echo "scale=6; $END_TIME - $START_TIME" | bc -l)
+            echo "Full-node recovery time: $DURATION seconds"
+            break
+        fi
+    fi
+done
