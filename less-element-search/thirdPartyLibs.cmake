14c2080213c233be0f9955bfbc445a84c0bbb24e
Keyun Cheng <keyun_cheng@qq.com>
2025-03-18 10:29:06 +0800

    update deps for find primitive-lib

--- less-element-search/thirdPartyLibs.cmake
+++ less-element-search/thirdPartyLibs.cmake
@@ -11,3 +11,17 @@ ExternalProject_Add (
     INSTALL_DIR ""
     INSTALL_COMMAND make install
 )
+
+## ISA-L
+ExternalProject_Add (
+    "isa-l" 
+    PREFIX "third-party/isa-l"
+    URL ${PROJECT_THIRD_PARTY_LIB_DIR}/isa-l-2.31.1.tar.gz
+    URL_MD5 851eb1b98da53c4dc8b94e0119106f03
+    UPDATE_COMMAND ""
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ./autogen.sh COMMAND ./configure ${THIRD_PARTY_CONFIG} --enable-shared=false --enable-static=true
+    BUILD_COMMAND make
+    INSTALL_DIR ""
+    INSTALL_COMMAND make install
+)
